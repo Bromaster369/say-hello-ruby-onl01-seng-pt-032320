a1ca34b3013efa294878585a567b56aa388b8003
Christian Pardo <chrispardopersonal@gmail.com>
2020-03-24 22:21:15 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
 def say_hello(name, language = "Hello Ruby Programmer!")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+  puts "Hello #{name}! and I am learning to program in #{language}."
 end
